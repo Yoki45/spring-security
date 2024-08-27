1b4417ec58563100a141dd7493ee1bad6d0e698b
okiyo <okiyostephen@gmail.com>
2024-08-27 09:43:50 +0300

    handle sessions and AuthenticationEvents incase of success or failure.

--- spring-security/src/main/java/com/spring/security/security/config/SecurityConfiguration.java
+++ spring-security/src/main/java/com/spring/security/security/config/SecurityConfiguration.java
@@ -1,13 +1,14 @@
 package com.spring.security.security.config;
 
+import com.spring.security.exception.CustomAccessDeniedHandler;
+import com.spring.security.exception.CustomBasicAuthenticationEntryPoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -17,15 +18,21 @@ public class SecurityConfiguration {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable());
+        //maximum session is used to handle  concurrent sessions.
+        http.sessionManagement(smc -> smc.invalidSessionUrl("/invalidSession").maximumSessions(3).maxSessionsPreventsLogin(true))
+
+                .requiresChannel(rcc -> rcc.anyRequest().requiresInsecure()) // Only HTTP
+//        http.requiresChannel(rcc -> rcc.anyRequest().requiresSecure()) // Only HTTPs
+                .csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable());
         http.authorizeHttpRequests((requests) -> requests
                 .requestMatchers("/accounts", "/balance", "/loans", "/cards").authenticated()
                 .requestMatchers("/notice", "/contact", "/error", "/actuator/**", "/swagger-ui/**", "/v3/api-docs/**",
-                        "/actuator/health","/register").permitAll());
+                        "/actuator/health","/register","/invalidSession").permitAll());
 //        http.formLogin(httpSecurityFormLoginConfigurer -> httpSecurityFormLoginConfigurer.disable());
 //        http.httpBasic(withDefaults());
         http.formLogin(withDefaults());
-        http.httpBasic(withDefaults());
+        http.httpBasic(httpSecurityHttpBasicConfigurer -> httpSecurityHttpBasicConfigurer.authenticationEntryPoint(new CustomBasicAuthenticationEntryPoint()));
+        http.exceptionHandling(ehc -> ehc.accessDeniedHandler(new CustomAccessDeniedHandler()));
         return http.build();
     }
 
